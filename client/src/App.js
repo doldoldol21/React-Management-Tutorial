b25023a591d0761c58eba7c46caf1e7276890308
doldoldol <haejehaeje@gmail.com>
2020-11-14 11:04:13 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 //import logo from './logo.svg';
 import Customer from './components/Customer';
+import CustomerAdd from './components/CustomerAdd';
 import './App.css';
 import { Component } from 'react';
 import Paper from '@material-ui/core/Paper';
@@ -26,18 +27,25 @@ const styles = theme => ({
 });
 
 class App extends Component{
+
   _isMounted = false;
 
-  state = {
-    isLoading: true,
-    customers: "",
-    completed: 0
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoading: true,
+      customers: "",
+      completed: 0
+    }
+    this.stateRefresh();
   }
 
-  componentDidMount() {
-    this._isMounted = true;
-
-    this.timer = setInterval(this.progress, 20);
+  stateRefresh = () => {
+    this.setState({
+      isLoading: true,
+      customers: "",
+      completed: 0
+    })
     this.callApi()
       .then(res => {
         if(this._isMounted){
@@ -46,11 +54,17 @@ class App extends Component{
         this.setState({customers: res})
       })
       .catch(err => console.log(err));
+      
+  }
+
+  componentDidMount() {
+    this._isMounted = true;
+    this.timer = setInterval(this.progress, 20);
   }
   componentWillUnmount() {
     this._isMounted = false;
   }
-
+  
   callApi = async () => {
     const response = await fetch('/api/customers');
     const body = await response.json();
@@ -65,33 +79,36 @@ class App extends Component{
   render() {
     const { classes } = this.props;
     return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.state.customers ? this.state.customers.map(c => { 
-              return ( 
-              <Customer key={c.id} id = {c.id} image = {c.image} name = {c.name} birthday = {c.birthday} gender ={c.gender} job = {c.job} /> 
-              );
-            }) : 
-            <TableRow>
-              <TableCell colSpan="6" align="center">
-                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
-              </TableCell>
-            </TableRow>
-            }
-          </TableBody>
-        </Table>
-      </Paper>
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.customers ? this.state.customers.map(c => { 
+                return ( 
+                <Customer key = {c.id} id = {c.id} image = {c.image} name = {c.name} birthday = {c.birthday} gender ={c.gender} job = {c.job} /> 
+                );
+              }) : 
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                </TableCell>
+              </TableRow>
+              }
+            </TableBody>
+          </Table>
+        </Paper>
+        <CustomerAdd stateRefresh={this.stateRefresh} />
+      </div>
     );
   }
 }
--- server.js
+++ server.js
@@ -20,6 +20,9 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
+const multer = require('multer');
+const upload = multer({dest: './upload'});
+
 app.get('/api/customers', (req, res) => {
     connection.query(
       "select * from customer",
@@ -27,6 +30,25 @@ app.get('/api/customers', (req, res) => {
         res.send(rows);
       }
     );
+});
+
+app.use('/image', express.static('./upload'));
+app.post('/api/customers', upload.single('image'), (req, res) => {
+  let sql = 'insert into customer values (null, ?, ?, ?, ?, ?)';
+  
+  // if(req.body.name === "undefined" || req.body.name === ''){
+  //   res.send({retCode: -1});
+  //   return;
+  // }
+  let image = req.file === undefined ? null : '/image/' + req.file.filename;
+  let name = req.body.name;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job;
+  let params = [image, name, birthday, gender, job];
+  connection.query(sql, params, (err, rows, fields) => {
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
